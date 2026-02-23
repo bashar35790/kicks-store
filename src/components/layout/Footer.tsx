aaa578d63cfe8a4e028feb52ecd3ae38e3b56233
Abul Bashar <bashar35790@gmail.com>
2026-02-23 22:41:53 +0600

    fixed footer sction

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,9 +5,10 @@ export const Footer = () => {
   return (
     <>
       {/* BLUE NEWSLETTER SECTION */}
-      <section className="bg-gradient-to-r from-[#4F6DFF] to-[#5C7CFA] text-white py-20 px-6 rounded-b-[40px]">
-        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-10">
-          
+      <section className=" text-white px-6 ">
+
+        <div className="bg-gradient-to-r from-[#4F6DFF] to-[#5C7CFA] p-16  max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-10 rounded-t-[40px]">
+
           {/* Left Content */}
           <div className="max-w-xl">
             <h2 className="text-4xl md:text-5xl font-extrabold leading-tight">
@@ -32,27 +33,23 @@ export const Footer = () => {
 
           {/* Right Big Text */}
           <div className="flex items-center gap-4 relative">
-          <div className="text-[12vw] font-extrabold text-white/90 tracking-tight hidden lg:block">
-            KICKS
-          </div>
-          <div className="w-7 h-7 absolute top-10 right-0 -translate-x-1/2 -translate-y-1/2 bg-amber-600 text-center rounded-full">
-            +
-          </div>
+            <div className="text-[12vw] font-extrabold text-white/90 tracking-tight hidden lg:block">
+              KICKS
+            </div>
+            <div className="w-7 h-7 absolute top-10 right-0 -translate-x-1/2 -translate-y-1/2 bg-amber-600 text-center rounded-full">
+              +
+            </div>
           </div>
         </div>
       </section>
 
       {/* FOOTER SECTION */}
-      <footer className="relative bg-[#1a1a1a] text-gray-300 pt-20 pb-12 overflow-hidden rounded-t-[40px] -mt-10">
-        
-        {/* Huge Background Text */}
-        <div className="absolute bottom-[-40px] left-1/2 -translate-x-1/2 text-[22vw] font-extrabold text-white/5 tracking-tighter pointer-events-none select-none whitespace-nowrap">
-          KICKS
-        </div>
+      <footer className="relative -top-20  text-gray-300 pt-20 pb-12 overflow-hidden rounded-t-[40px] -mt-10">
 
-        <div className="max-w-7xl mx-auto px-6 relative z-10">
+
+        <div className="bg-[#1a1a1a] p-16 rounded-t-[40px] max-w-7xl mx-auto px-6 relative z-10 pb-50 relative">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16">
-            
+
             {/* About Us */}
             <div>
               <h3 className="text-orange-400 text-2xl font-bold mb-6">
@@ -114,6 +111,12 @@ export const Footer = () => {
               </div>
             </div>
           </div>
+
+          {/* Huge Background Text */}
+          <div className="absolute top-80%  z-100 left-2.5 opacity-100">
+            <img src="/footerlogo.png" alt="footerlogo" className="cover" />
+          </div>
+
         </div>
       </footer>
     </>
